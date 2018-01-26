18d2c6241d7626f15189342119a088d07b19bdf8
Rinalds Pudulis <rpudulis@gmail.com>
2018-01-26 12:20:03 +0200

    feat: Added basic navigation - TabNavigator

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TabNavigator } from "react-navigation";
+import Home from "./components/Home";
+import Notifications from "./components/Notifications";
+
+import { red } from "./utils/colors";
+
+const Tabs = TabNavigator(
+  {
+    Home: {
+      screen: Home
+    },
+    Notifications: {
+      screen: Notifications
+    }
+  },
+  {
+    tabBarPosition: "top",
+    animationEnabled: true,
+    tabBarOptions: {
+      activeTintColor: red
+    }
+  }
+);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }}>
+        <Tabs />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
---
+++ components/Notifications.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+class Notifications extends Component {
+  render() {
+    return (
+      <View>
+        <Text>There are notifications</Text>
+      </View>
+    );
+  }
+}
+
+export default Notifications;
