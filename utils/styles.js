def23fdad16682b5b1c86fa4039db78c65c53cba
Rinalds Pudulis <rpudulis@gmail.com>
2018-02-02 13:52:35 +0200

    refactor: Created separate styles file

---
+++ utils/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 50
+  },
+  textInput: {
+    padding: 10,
+    fontSize: 20,
+    width: "100%",
+    marginBottom: 50
+  }
+});
