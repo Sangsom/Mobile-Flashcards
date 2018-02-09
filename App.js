19cb143af79917d8ace4453b43bc7abb8751a79e
Rinalds Pudulis <rpudulis@gmail.com>
2018-02-09 11:22:08 +0200

    feat: Installed Redux Thunk

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import reducers from "./reducers";
 import Navigator from "./components/Navigator";
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 export default class App extends Component {
   render() {
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <View style={{ flex: 1 }}>
           <Navigator />
         </View>
