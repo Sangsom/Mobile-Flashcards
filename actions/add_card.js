a62f2ca44f9955895a6ba5534d12b14781195cf0
Rinalds Pudulis <rpudulis@gmail.com>
2018-02-09 11:29:25 +0200

    feat: Added dispatch to action creators

--- actions/add_card.js
+++ actions/add_card.js
@@ -1,6 +1,12 @@
 import { ADD_CARD } from "./actions";
 
 export function addCard(card) {
+  return dispatch => {
+    dispatch(addCardAsync(card));
+  };
+}
+
+function addCardAsync(card) {
   return {
     type: ADD_CARD,
     card
--- actions/add_deck.js
+++ actions/add_deck.js
@@ -1,6 +1,12 @@
 import { ADD_DECK } from "./actions";
 
 export function addDeck(deck) {
+  return dispatch => {
+    dispatch(addDeckAsync(deck));
+  };
+}
+
+function addDeckAsync(deck) {
   return {
     type: ADD_DECK,
     deck
